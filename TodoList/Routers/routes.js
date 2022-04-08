04927ed941f8222c2bb431934e67ad856b827f80
QuangDung <dung05042000@gmail.com>
2022-04-08 14:31:36 +0700

    added feature Sign/Login/ChangePassword

--- TodoList/Routers/routes.js
+++ TodoList/Routers/routes.js
@@ -14,6 +14,7 @@ const app = express();
 //USERS
 app.post("/createAccount", ControllerAccounts.createAccount);
 app.post("/login", ControllerAccounts.login)
+app.put("/changepassword",ControllerAccounts.changePassword);
 //SONGS
 app.post("/createSong", ControllerSongs.createMusic)
 //STUDENTS
